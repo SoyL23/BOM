189943667c04a63384dd9f9e0ff54c2f4aaea4a1
Soporte Soporte <Soporte@SMART.local>
2024-03-14 14:26:48 -0500

    Vista Login

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from config.db import db
 class Config_Development:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql://root:SmartCol24*@localhost/bom_entrenamiento"
+    SQLALCHEMY_DATABASE_URI = "mysql://Lyrion:Sm4rtCol24*@localhost/bom_entrenamiento"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSONIFY_PRETTYPRINT_REGULAR = True
     SECRET_KEY = "*b0m_3ntr3n4m13nt0_db*"
